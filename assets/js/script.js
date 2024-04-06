ae2bf6858510b35afa576d9c5d3fece608d5a165
Holinda Beshiri <holinda_b@hotmail.com>
2024-04-06 20:14:35 +0000

    Add the variable for getting the Start button previously hidden to be shown when closing the instructions modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Get the modal - W3 Schools
+// Get the modal - W3 Schools adjusted and added
 let modal = document.getElementById("myRules");
 
 // Get the button that opens the modal
@@ -8,21 +8,26 @@ let openButton = document.getElementById("rules");
 let span = document.getElementsByClassName("close")[0];
 
 let holder = document.getElementById("holder");
+
+let button = document.getElementById ("start");
 // When the user clicks on the button, open the modal
 rules.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modal and open Start button
 span.onclick = function() {
   modal.style.display = "none";
   openButton.style.display="none";
+  button.style.display = "block";
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+
   }
 } 
 // End of W3 Schools code - adjusted
@@ -51,7 +56,7 @@ function hideEnter() {
     hideUsername.style.display = "none";
    }
 
-
+   
 
 // Adding questions - Question source : https://www.quiztriviagames.com/multiple-choice-trivia-questions/
 let questions = [
